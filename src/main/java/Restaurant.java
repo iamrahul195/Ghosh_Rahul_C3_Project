88e9d27126aa5ad4e5ff1c7460753ae255e64d31
iamrahul195 <iamrahulit195@gmail.com>
2022-02-07 13:16:34 +0530

    Part 2: Solution

---
+++ src/main/java/Restaurant.java
@@ -0,0 +1,93 @@
+
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Restaurant {
+    private String name;
+    private String location;
+    public LocalTime openingTime;
+    public LocalTime closingTime;
+    
+    private List<Item> menu = new ArrayList<Item>();
+
+    public Restaurant(String name, String location, LocalTime openingTime, LocalTime closingTime) {
+        this.name = name;
+        this.location = location;
+        this.openingTime = openingTime;
+        this.closingTime = closingTime;
+    }
+
+    public boolean isRestaurantOpen() {
+        LocalTime currentTime = getCurrentTime();
+    	
+        Boolean target = ( 
+            currentTime.isAfter( openingTime ) && currentTime.isBefore( closingTime ) 
+            ) || (currentTime.equals( openingTime )); 
+
+        return target;
+    	
+       
+        //DELETE ABOVE STATEMENT AND WRITE CODE HERE
+    }
+
+    public LocalTime getCurrentTime(){ return  LocalTime.now(); }
+
+    public List<Item> getMenu() {
+        return menu;
+        //DELETE ABOVE RETURN STATEMENT AND WRITE CODE HERE
+    }
+
+    private Item findItemByName(String itemName){
+        for(Item item: menu) {
+            if(item.getName().equals(itemName))
+                return item;
+        }
+        return null;
+    }
+
+    public void addToMenu(String name, int price) {
+        Item newItem = new Item(name,price);
+        menu.add(newItem);
+    }
+    
+    public void removeFromMenu(String itemName) throws itemNotFoundException {
+
+        Item itemToBeRemoved = findItemByName(itemName);
+        if (itemToBeRemoved == null)
+            throw new itemNotFoundException(itemName);
+
+        menu.remove(itemToBeRemoved);
+    }
+    public void displayDetails(){
+        System.out.println("Restaurant:"+ name + "\n"
+                +"Location:"+ location + "\n"
+                +"Opening time:"+ openingTime +"\n"
+                +"Closing time:"+ closingTime +"\n"
+                +"Menu:"+"\n"+getMenu());
+
+    }
+
+    public String getName() {
+        return name;
+    }
+    // my method
+    // public Double OrderTotal(String[] ItemNameArray) {
+    // 	int size = ItemNameArray.length;
+    // 	Double totalPrice = 0.0;
+    	
+    // 	for(int i = 0 ;i<size;i++) {
+    		
+    // 		Item item = findItemByName(ItemNameArray[i]);
+    //         totalPrice = totalPrice + item.getPrice();
+    		
+    		
+    // 	}
+    	
+    //     return totalPrice;
+    // }
+    
+    
+
+}
---
+++ src/main/java/restaurantNotFoundException.java
@@ -0,0 +1,7 @@
+
+
+public class restaurantNotFoundException extends RuntimeException {
+    public restaurantNotFoundException(String restaurantName) {
+        super(restaurantName);
+    }
+}
